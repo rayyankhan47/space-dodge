6dcb952a3715fe2c57ca5b635c770ea7e3681b04
AhmedSalafi <ryankhann47@gmail.com>
2025-08-06 05:19:24 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Rain Dodge")
+
+BG = pygame.transform.scale(pygame.image.load("background-image.jpg"), (WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        draw()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
