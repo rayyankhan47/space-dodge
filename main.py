a7cdd7e6e1931c428146c6a11b9f3a51eccc1fd4
AhmedSalafi <ryankhann47@gmail.com>
2025-08-07 03:22:29 -0400

    creating ticking clock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -13,8 +14,13 @@ PLAYER_HEIGHT = 60
 
 PLAYER_VEL = 5
 
-def draw(player):
+FONT = pygame.font.SysFont("Arial", 30)
+
+def draw(player, elapsed_time):
     WIN.blit(BG, (0, 0))
+
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
 
     pygame.draw.rect(WIN, (255, 0, 0), player)
 
@@ -27,9 +33,13 @@ def main():
                         PLAYER_WIDTH, PLAYER_HEIGHT)
     
     clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
+
 
     while run:
         clock.tick(60) # while loop will only run 60 times in one second
+        elapsed_time = time.time() - start_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,7 +52,7 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + player.width + PLAYER_VEL <= WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        draw(player, elapsed_time)
 
     pygame.quit()
 
